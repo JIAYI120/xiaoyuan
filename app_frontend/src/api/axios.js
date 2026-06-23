5de4f339a0ee377932de2ac1f4e934af032e38e3
JIAYI120 <2377200640@qq.com>
2026-06-23 17:53:21 +0800

    Update axios.js

--- app_frontend/src/api/axios.js
+++ app_frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://appbackend-topaz.vercel.app/api',
+  baseURL: 'appbackend-zjy.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
