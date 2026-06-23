ffd0e5df9b5d583d83d88ad521e1fda739316b44
JIAYI120 <2377200640@qq.com>
2026-06-23 16:33:25 +0800

    Update axios.js

--- app_frontend/src/api/axios.js
+++ app_frontend/src/api/axios.js
@@ -1,23 +1,21 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: 'https://appbackend-topaz.vercel.app/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 apiClient.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers['x-auth-token'] = token;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['x-auth-token'] = token;
     }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default apiClient;
